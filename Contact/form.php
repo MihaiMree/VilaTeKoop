76d49c472488fcebc651ff2931ab417d8a113723
Kaan Yarpuz <89530@glr.nl>
2023-05-31 15:28:33 +0200

    veranderingen gemaakt

--- Contact/form.php
+++ Contact/form.php
@@ -6,69 +6,76 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <title>Document</title>
+    <style>
+    .btn-outline-light-blue {
+        border-color: #6BCAFF;
+    }
+
+    .text-light-blue {
+        color:  #6BCAFF;
+    }
+
+
+</style>
+    
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="#">Villa Sales</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#about">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#villas">Villas</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#contact">Contact</a>
-                    </li>
-                </ul>
-            </div>
+    <div class="container">
+        <a class="navbar-brand" href="../homepage.html">Villa Sales</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="../homepage.html">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#villas">Villas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="form.php">Contact</a>
+                </li>
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
 
-<div style="padding:60px"></div>
+<div class="container mt-5">
+    <h3 class="text-center">Neem contact met ons</h3>
+    <p class="text-center">Heeft u vragen? Stel ze gerust hier en wij kunnen u beantwoorden!</p>
 
-<h3 class="text-center">Contact ons</h3>
-<div class="container">
-<form method="POST" action="process.php">
-
-<div class="form-group">
-<label for="naam">Naam</label>
-<input type="text" name="naam" id="naam" class="form-control" required>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <form method="POST" action="process.php">
+                <div class="form-group">
+                    <label for="naam">Naam</label>
+                    <input type="text" name="naam" id="naam" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="adres">Adres</label>
+                    <input type="text" name="adres" id="adres" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="telefoonnummer">Telefoonnummer</label>
+                    <input type="text" name="telefoonnummer" id="telefoonnummer" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">E-mail</label>
+                    <input type="email" name="email" id="email" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="vraag">Vraag</label>
+                    <input type="text" name="vraag" id="vraag" class="form-control" required>
+                </div>
+                <div class="text-end">
+                <button type="submit" class="btn btn-transparent btn-outline-light-blue text-light-blue">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
-
-<div class="form-group">
-<label for="adres">Adres</label>
-<input type="text" name="adres" id="adres" class="form-control" required>
-</div>
-
-<div class="form-group">
-<label for="telefoonnummer">Telefoonnummer</label>
-<input type="text" name="telefoonnummer" id="telefoonnummer" class="form-control" required>
-</div>
-
-<div class="form-group">
-<label for="email">E-mail</label>
-<input type="email" name="email" id="email" class="form-control" required>
-</div>
-
-<div class="form-group">
-<label for="vraag">Vraag</label>
-<input type="text" name="vraag" id="vraag" class="form-control" required>
-</div>
-
-<div class="form-group">
-<button type="submit" class="btn btn-success">Submit</button>
-</div>
-
-</form>
-
-</div>
-
 </body>
 </html>
