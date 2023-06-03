5e2e7ca5240abaa31d26a19751185e2a86a029db
Kaan Yarpuz <89530@glr.nl>
2023-06-03 15:00:23 +0200

    contactpagina werkt (wordt opgeslagen in db)

--- Contact/process.php
+++ Contact/process.php
@@ -1,17 +1,36 @@
-<?php 
-include("config.php");
+<?php
+// Establish database connection
+$hostname = "localhost";
+$username = "contact";
+$password = "h0m2Di28?";
+$database = "Database89530";
+$mysqli = new mysqli($hostname, $username, $password, $database);
 
-extract($_POST);
-$query = "INSERT INTO  `contact-data` (`naam`,`adres`,`telefoonnummer`,`email`,`vraag`) VALUES ('".$naam."','".$adres."','".$telefoonnummer."','".$email."', '".$vraag."')";
-$result = $mysqli->query($query);
-if(!$result){
-    echo "Something went wrong";
+// Check the connection
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
 }
 
-echo "thanks for submiting your quyery";
+// Retrieve form data
+$naam = $_POST['naam'];
+$adres = $_POST['adres'];
+$telefoonnummer = $_POST['telefoonnummer'];
+$email = $_POST['email'];
+$vraag = $_POST['vraag'];
+
+// Sanitize and validate the data (you may use additional validation)
+
+// Construct the SQL query
+$sql = "INSERT INTO contact_form (naam, adres, telefoonnummer, email, vraag) VALUES ('$naam', '$adres', '$telefoonnummer', '$email', '$vraag')";
+
+// Execute the query
+if ($mysqli->query($sql)) {
+    echo "Form is verzonden.";
+} else {
+    echo "Error: " . $mysqli->error;
+}
+
+// Close the database connection
 $mysqli->close();
-
-print_r($_POST);
-
-
 ?>
