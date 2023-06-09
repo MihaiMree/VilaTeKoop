52de4285ad7ad9e637f324508867a752c5d4d0cf
MihaiMree <122447324+MihaiMree@users.noreply.github.com>
2023-06-09 14:03:46 +0200

    alles

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost"; // Replace with your server name
+$username = "mihai"; // Replace with your MySQL username
+$password = "2Fp0bb58*"; // Replace with your MySQL password
+$dbname = "ftp89621"; // Replace with your MySQL database name
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+
